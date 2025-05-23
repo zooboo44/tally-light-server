3f1813ee632885e886079f701af8afffb433d1f8
Christian Ziobro <ziobro44@gmail.com>
2025-05-22 22:16:56 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tally-light-server
+//
+//  Created by CCPV on 5/22/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
